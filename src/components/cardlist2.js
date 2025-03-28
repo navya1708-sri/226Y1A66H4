6e3a201f30ee104a30d24fb7fae9ce5616485991
navya1708-sri <sudhamnavyasri8@gmail.com>
2025-03-28 15:00:53 +0530

    Update cardlist2.js

--- src/components/cardlist2.js
+++ src/components/cardlist2.js
@@ -5,7 +5,7 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-
+//react
 export default class CardList2 extends Component {
   render() {
     return (
